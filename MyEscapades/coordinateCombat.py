45df7eb6f0ad293898538354ef0d5f8c53365dfa
LIHTU <robinanelson@gmail.com>
2015-06-04 13:41:49 -0700

    updated coordinates

---
+++ MyEscapades/coordinateCombat.py
@@ -0,0 +1,17 @@
+# Coordinate Combat
+# In this minigame the user will be prompted to
+# defend themself against mobs by entering in coordinates
+# for defensive strucutes and weapons, such as fire or
+# trap pits, or TNT.
+
+'''
+1. figure out if we can turn mobs on, and make them hostile.
+2. learn and implement chat commands.
+3. Design minigame and sequencing.
+    where
+    which mobs
+4. Can we detect whether a mob entity is dead or alive?
+5. Maybe we could simulate a mob with moving block monsters, like
+    the ufo in adventure 8.
+'''
+
